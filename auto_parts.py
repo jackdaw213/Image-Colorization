99412fe599edb24dee2760b7517e50e9af3043d8
jackdaw213 <103126046+jackdaw213@users.noreply.github.com>
2023-12-06 11:17:17 +1100

    Add CUNet model

--- auto_parts.py
+++ auto_parts.py
@@ -65,7 +65,7 @@ class LatentSpace(nn.Module):
         )
     def forward(self, inp):
         return self.seq(inp)
-    
+
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
@@ -73,6 +73,83 @@ class OutConv(nn.Module):
             nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0),
             nn.BatchNorm2d(out_channels),
             nn.ReLU()
+        )
+    def forward(self, inp):
+        return self.seq(inp)
+
+class CEncoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, stride=1):
+        super().__init__()
+        self.seq = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+            nn.Sigmoid(),
+            nn.BatchNorm2d(out_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+            nn.Sigmoid(),
+            nn.BatchNorm2d(out_channels),
+            nn.Dropout2d(0.1)
+        )
+        self.down_conv = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=2)
+    def forward(self, inp):
+        features = self.seq(inp)
+        # Features is for concatenating with the decoder blocks
+        # Down is for the next encoder block
+        down = self.down_conv(features)
+        return features, down
+
+class CDecoderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, stride=1):
+        super().__init__()
+        # Upscale and half the number of features
+        self.trans_conv = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=4, stride=2)
+        self.seq = nn.Sequential(
+            # so that when we concat the encoder block's features
+            # the amount of input features stays the same
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+            nn.Sigmoid(),
+            nn.BatchNorm2d(out_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride),
+            nn.Sigmoid(),
+            nn.BatchNorm2d(out_channels),
+            nn.Dropout2d(0.1)
+        )
+    def forward(self, inp, con_channels):
+        up = self.trans_conv(inp)
+
+        # We need to pad the mask when we concatenating
+        # upscaled features that were previously 
+        # downscaled from odd dimension features
+        # For example: 25 -> down -> 12 -> up -> 24 -> pad -> 25
+        diffY = con_channels.size()[2] - up.size()[2]
+        diffX = con_channels.size()[3] - up.size()[3]
+        up = torch.nn.functional.pad(up, [diffX // 2, diffX - diffX // 2,
+                                          diffY // 2, diffY - diffY // 2])
+        
+        cat = torch.cat([up, con_channels], dim=1)
+        return self.seq(cat)
+    
+class ExtendedConvolution(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=2, stride=1, dilation=2):
+        super().__init__()
+        self.seq = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride, dilation=dilation),
+            nn.Sigmoid(),
+            nn.BatchNorm2d(out_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding, stride=stride, dilation=dilation),
+            nn.Sigmoid(),
+            nn.BatchNorm2d(out_channels),
+            nn.Dropout2d(0.1)
+        )
+    def forward(self, inp):
+        return self.seq(inp)
+
+class COutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.seq = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0),
+            nn.BatchNorm2d(out_channels),
+            nn.Tanh()
         )
     def forward(self, inp):
         return self.seq(inp)
--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import auto_parts as ap
 import torch.nn.functional as F
 
-class CAE(nn.Module):
+class UNet(nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -34,6 +34,38 @@ class CAE(nn.Module):
         r5_d = self.d5(ls1, r5_e_f)
         r4_d = self.d4(r5_d, r4_e_f)
         r3_d = self.d3(r4_d, r3_e_f)
+        r2_d = self.d2(r3_d, r2_e_f)
+        r1_d = self.d1(r2_d, r1_e_f)
+
+        return self.out(r1_d)
+    
+#https://www.mdpi.com/2073-8994/14/11/2295    
+class CUNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.e1 = ap.CEncoderBlock(1, 64)
+        self.e2 = ap.CEncoderBlock(64, 128)
+        self.e3 = ap.CEncoderBlock(128, 256)
+
+        self.ec1 = ap.ExtendedConvolution(256, 512)
+        self.ec2 = ap.ExtendedConvolution(512, 512)
+
+        self.d3 = ap.CDecoderBlock(512, 256)
+        self.d2 = ap.CDecoderBlock(256, 128)
+        self.d1 = ap.CDecoderBlock(128, 64)
+
+        self.out = ap.OutConv(64, 2)
+
+    def forward(self, x):
+        r1_e_f, r1_e_d = self.e1(x)
+        r2_e_f, r2_e_d = self.e2(r1_e_d)
+        r3_e_f, r3_e_d = self.e3(r2_e_d)
+
+        ec1 = self.ec1(r3_e_d)
+        ec2 = self.ec2(ec1)
+
+        r3_d = self.d3(ec2, r3_e_f)
         r2_d = self.d2(r3_d, r2_e_f)
         r1_d = self.d1(r2_d, r1_e_f)
 
