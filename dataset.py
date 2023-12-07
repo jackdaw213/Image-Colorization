a1748c6072e7519a46b178e814b990fdea7a64ed
jackdaw213 <103126046+jackdaw213@users.noreply.github.com>
2023-12-08 09:15:12 +1100

    Change dataset class a bit

--- dataset.py
+++ dataset.py
@@ -7,18 +7,25 @@ from torchvision.io import read_image
 from PIL import Image
 
 class ColorDataset(torch.utils.data.Dataset):
-    def __init__(self, image_dir):
+    def __init__(self, image_dir, do_transform=False):
         self.names = os.listdir(image_dir)
         self.image_dir = image_dir
-
+        self.do_transform = do_transform
+        self.transform = transforms.Compose([
+            transforms.Resize((320, 240), antialias=True)
+        ])
     def __len__(self):
         return len(self.names)
 
     def __getitem__(self, index):
         image_path = os.path.join(self.image_dir, self.names[index])
 
-        color_img = torch.from_numpy(rgb2lab(read_image(image_path).permute(1, 2, 0))).permute(2, 0, 1).float()
-        color = color_img[1:, :, :]
-        black = color_img[0, :, :].unsqueeze(0)
+        image = rgb2lab(Image.open(image_path).convert("RGB"))
+        image = torch.from_numpy(image).permute(2,0,1).float()
+        if self.do_transform:
+            image = self.transform(image)
 
+        color = image[1:, :, :]
+        black = image[0, :, :].unsqueeze(0)
+        
         return black, color
