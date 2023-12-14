03deece476b6a9e2780387be07da495fbef3b9a6
jackdaw213 <103126046+jackdaw213@users.noreply.github.com>
2023-12-14 11:16:03 +1100

    Main.py refactoring, added some flags for performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import torch
 from torch.utils.data import DataLoader
-import torch.nn as nn
 import nvidia_dlprof_pytorch_nvtx
 
 import dataset
@@ -10,26 +9,43 @@ import auto_parts
 
 torch.backends.cudnn.allow_tf32 = True
 torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.benchmark = True
+torch.set_float32_matmul_precision("high")
 
 #nvidia_dlprof_pytorch_nvtx.init()
 
-NUM_EPOCHS = 300
-BATCH_SIZE = 32
+NUM_EPOCHS = 100
+BATCH_SIZE = 8
 
 print("Importing dataset")
-train_dataset = dataset.ColorDataset('data/train')
-val_dataset = dataset.ColorDataset('data/val')
+train_dataset = dataset.ColorDataset('data/train', True)
+val_dataset = dataset.ColorDataset('data/val', True)
 
 print("Init dataloader")
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
-val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+train_loader = DataLoader(
+    train_dataset, 
+    batch_size=BATCH_SIZE, 
+    shuffle=True, 
+    num_workers=4, 
+    pin_memory=True)
+val_loader = DataLoader(
+    val_dataset, 
+    batch_size=BATCH_SIZE, 
+    shuffle=True, 
+    num_workers=4, 
+    pin_memory=True)
 
-model = model.CAE()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-#loss = nn.MSELoss()
+model = model.UNet()
+model._initialize_weights()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.6)
+#optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+
+#loss = nn.MSELoss().cuda()
 loss = auto_parts.HuberLoss().cuda()
+
 #torchinfo.summary(model, (1, 224, 224), batch_dim = 0, col_names = ('input_size', 'output_size', 'num_params', 'kernel_size', 'mult_adds'), verbose = 1)
 
 print("Training...")
 #with torch.autograd.profiler.emit_nvtx():
-train.train_model(model, optimizer, loss, train_loader, val_loader, NUM_EPOCHS, 10, 100)
+train.train_model(model, optimizer, loss, train_loader, val_loader, NUM_EPOCHS, 1, 5)
