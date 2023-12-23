e33b0165827c40986885c778b764fc4a0655ba12
jackdaw213 <103126046+jackdaw213@users.noreply.github.com>
2023-12-23 16:54:45 +1100

    Added Nvidia DALI, cuCIM, for faster data preprocessing

--- dataset.py
+++ dataset.py
@@ -2,9 +2,14 @@ import torch
 import os
 import torchvision.transforms.functional as f
 import torchvision.transforms as transforms
-from skimage.color import rgb2lab
+from cucim.skimage.color import rgb2lab
 from torchvision.io import read_image
 from PIL import Image
+
+import cv2
+import nvidia.dali.types as types
+from nvidia.dali.pipeline import pipeline_def
+import nvidia.dali.fn as fn
 
 class ColorDataset(torch.utils.data.Dataset):
     def __init__(self, image_dir, do_transform=False):
@@ -28,4 +33,32 @@ class ColorDataset(torch.utils.data.Dataset):
         color = image[1:, :, :]
         black = image[0, :, :].unsqueeze(0)
         
+        return black, color
+
+    @staticmethod
+    def smt(image):
+        print(image.shape)
+        return image
+    
+    @staticmethod
+    @pipeline_def(device_id=0)
+    def dali_pipeline(image_dir, do_transform=True):
+        images, _ = fn.readers.file(file_root=image_dir, 
+                                         file_list=image_dir+"/labels.txt",
+                                         random_shuffle=True, 
+                                         name="Reader")
+        
+        images = fn.decoders.image(images, device="mixed", output_type=types.RGB)
+        images = fn.python_function(images, function=rgb2lab)
+
+        if do_transform:
+            images = fn.resize(images, size=(320, 240))
+
+        images = fn.transpose(images, perm=[2, 0, 1])
+        
+        #assd = fn.python_function(images, function=ColorDataset.smt)
+        
+        color = images[1:, :, :]
+        black = fn.expand_dims(images[0, :, :], axes=0)
+
         return black, color
