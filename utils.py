9c30176463e5655c5fb7a511dc30a48b519b6855
jackdaw213 <103126046+jackdaw213@users.noreply.github.com>
2024-04-16 21:21:55 +1000

    Added test_style_model and removed all unnecessary functions

--- utils.py
+++ utils.py
@@ -1,23 +1,16 @@
 import os
-import shutil
 import sys
 import random
-import filetype
 
 import torch
-import torchmetrics
-from torchvision.io import read_image
 
 import matplotlib.pyplot as plt
 from skimage.color import lab2rgb
 from skimage.color import rgb2lab
 
-import tqdm as tq
 import numpy as np
-import torch.nn as nn
 from PIL import Image
 import PIL
-from PIL import ImageFile
 import torchvision.transforms.functional as F
 
 def image_grid(**kwargs):
@@ -54,77 +47,70 @@ def concat_l_and_to_rgb(l, ab_shape):
     l = torch.cat([l, torch.zeros(ab_shape)], 0)
     return lab2rgb(l.permute(1, 2, 0))
 
-def test_learnability(model, learning_rate, image_path, n_epochs):
-    device = torch.device("cpu")
-    if torch.cuda.is_available():
-        device = torch.device("cuda:0")
-        torch.cuda.set_device(device)
-
-    l, ab = rgb_to_l_ab(image_path)
-    ab = ab.unsqueeze(0).float().cuda()
-    l = l.unsqueeze(0).float().cuda()
-
-    epoch_loss = torchmetrics.MeanMetric().to(device)
-
-    model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    loss_func = nn.SmoothL1Loss().cuda()
-
-    train_list = []
-
-    model.train()
-    for epoch in tq.tqdm(range(n_epochs), total=n_epochs, desc='Epochs'):
-    
-        output = model(l)
-        loss = loss_func(output, ab)
-
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        epoch_loss(loss)
-        train_list.append(epoch_loss.compute().cpu())
-
-    model.eval()
-    with torch.no_grad():
-        out = model(l)
-        l = l.cpu().squeeze(0)
-        out = out.cpu().squeeze(0)
-        ab = ab.cpu().squeeze(0)
-        result = l_ab_to_rgb(l, out)
-        ground = l_ab_to_rgb(l, ab)
-
-        compare_img((concat_l_and_to_rgb(l, ab.shape), result, ground))
-
-    plt.plot(train_list, label='train_loss')
-    plt.legend()
-    plt.show()
-
-def test_trained_model(model, test_images_path, num_samples=8):
+def test_color_model(model, test_images_path, num_samples=8):
     images = os.listdir(test_images_path)
-    selected_images = np.random.choice(images, num_samples, replace=False)
-    image_list = []
-    for image_name in selected_images:   
+    images = np.random.choice(images, num_samples, replace=False)
+
+    input = []
+    output = []
+    ground_truth = []
+
+    for image_name in images:   
         image_path = os.path.join(test_images_path, image_name) 
-        l, ab = rgb_to_l_ab(image_path)
+        pil_img = Image.open(image_path).convert("RGB")
+
+        ground_truth.append(pil_img)
+
+        l, ab = rgb_to_l_ab(pil_img)
+
         mask = (torch.rand((ab.shape[1], ab.shape[2])) > 0.95).float()
         inp = torch.cat([l, ab * mask], dim=0)
+        input.append(lab2rgb(inp.permute(1, 2, 0)))
+
         ab = ab.unsqueeze(0).float()
         inp = inp.unsqueeze(0).float()
+
         model.eval()
         with torch.no_grad():
             out = model(inp)
-            out = out.squeeze(0)
-            ab = ab.squeeze(0)
-            image_list.append((concat_l_and_to_rgb(l, ab.shape), l_ab_to_rgb(l, out), l_ab_to_rgb(l, ab)))
-    compare_img_table(image_list, num_samples)              
+        out = out.squeeze(0)
+        ab = ab.squeeze(0)
+        output.append(l_ab_to_rgb(l, out))
 
-def plot_loss_history(state_file):
-    state = torch.load(state_file)
-    plt.plot(state["train_list"], label='train_loss')
-    plt.plot(state["val_list"],label='val_loss')
-    plt.legend()
-    plt.show()
+    image_grid(Input=input, Output=output, Ground_Truth=ground_truth)     
+
+def test_style_model(model, con_images_path, sty_images_path, num_samples=8):
+    cons = os.listdir(con_images_path)
+    cons = np.random.choice(cons, num_samples, replace=False)
+
+    stys = os.listdir(sty_images_path)
+    stys = np.random.choice(stys, num_samples, replace=False)
+
+    con_images = []
+    sty_images = []
+    output = []
+
+    for con, sty in zip(cons, stys):   
+        con = os.path.join(con_images_path, con) 
+        con = Image.open(con).convert("RGB")
+        con_images.append(con)
+        con = norm_pil(con)
+
+        sty = os.path.join(sty_images_path, sty) 
+        sty = Image.open(sty).convert("RGB")
+        sty_images.append(sty)
+        sty = norm_pil(sty)
+
+        con = con.unsqueeze(0).float()
+        sty = sty.unsqueeze(0).float()
+
+        model.eval()
+        with torch.no_grad():
+            out = model(con, sty)
+        out = denorm(out.squeeze())
+        output.append(F.to_pil_image(out))
+
+    image_grid(Content=con_images, Style=sty_images, Output=output)            
 
 def save_train_state(model, optimizer, epoch, path):
     # This one is for resuming training
@@ -147,13 +133,7 @@ def load_train_state(path):
         return state["model"], state["optimizer"], state["epoch"]
     except Exception as e:
         print(e)
-        print("Error encounted when trying to load train state, trying backup")
-        try:
-            torch.load(path + ".bak")
-            return state["model"], state["optimizer"], state["epoch"]
-        except:
-            print(e)
-            sys.exit("Backup train state failed, existing")
+        sys.exit("Backup train state failed, existing")
 
 def norm(tensor):
     return F.normalize(tensor, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
@@ -167,62 +147,6 @@ def denorm(tensor):
     mean = torch.Tensor([0.485, 0.456, 0.406]).reshape(-1, 1, 1)
     res = torch.clamp(tensor * std + mean, 0, 1)
     return res
-
-def split_images(input_folder, output_folder, train_ratio=0.8, val_ratio=0.15, test_ratio=0.05):
-    if train_ratio + val_ratio + test_ratio != 1:
-        raise ValueError("Ratio sum must be 1")
-    # Create output folders if they don't exist
-    train_folder = os.path.join(output_folder, 'train')
-    val_folder = os.path.join(output_folder, 'val')
-    test_folder = os.path.join(output_folder, 'test')
-
-    os.makedirs(train_folder, exist_ok=True)
-    os.makedirs(val_folder, exist_ok=True)
-    os.makedirs(test_folder, exist_ok=True)
-
-    # Get a list of all image files in the input folder
-    image_files = [f for f in os.listdir(input_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
-
-    # Shuffle the list of image files
-    random.shuffle(image_files)
-
-    # Calculate the number of images for each set
-    total_images = len(image_files)
-    train_count = int(total_images * train_ratio)
-    val_count = int(total_images * val_ratio)
-    test_count = total_images - train_count - val_count
-
-    # Copy images to the corresponding folders
-    for i, image_file in enumerate(image_files):
-        source_path = os.path.join(input_folder, image_file)
-
-        if i < train_count:
-            destination_folder = train_folder
-        elif i < train_count + val_count:
-            destination_folder = val_folder
-        else:
-            destination_folder = test_folder
-
-        destination_path = os.path.join(destination_folder, image_file)
-        shutil.copy(source_path, destination_path)
-
-    print(f"Splitting complete. Train: {train_count}, Val: {val_count}, Test: {test_count}")
-
-def create_small_dataset(original_folder, target_folder, split_ratio=0.1):
-    if not os.path.exists(target_folder):
-        os.makedirs(target_folder)
-
-    all_files = os.listdir(original_folder)
-
-    num_files = len(all_files)
-    num_files = int(num_files * split_ratio)
-
-    small_files = random.sample(all_files, num_files)
-
-    for file_name in small_files:
-        source_path = os.path.join(original_folder, file_name)
-        target_path = os.path.join(target_folder, file_name)
-        shutil.copyfile(source_path, target_path)
 
 def mean_std(input):
     mean = torch.mean(input, dim=(2, 3), keepdims=True)
